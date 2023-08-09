169ee7ccb4e34832d5c3f9f625b94d8e3485394e
William Calabrase <calabrase.william@gmail.com>
2023-08-09 11:54:51 -0400

    minimal changes

--- vue/src/components/EditProfile.vue
+++ vue/src/components/EditProfile.vue
@@ -22,6 +22,9 @@
 </template>
 
 <script>
+import UserService from '../services/UserService';
+import authService from '../services/AuthService';
+
 export default {
   name: 'edit-profile',
   data() {
@@ -36,14 +39,43 @@ export default {
   },
   methods:{
     updateUser(){
-      this.$store.commit("UPDATE_USER",this.user)
+      this.$store.commit("UPDATE_USER",this.user);
       // this.user=if(!this.user.name==='');
       // this.user=if(!this.user.email==='');
       this.user={
         name: this.$store.state.user.name,
         email: this.$store.state.user.email,
-      }
-      // this.$router.push('/profile');
+      };
+      authService
+        .login(this.user)
+        .then((response) => {
+          if (response.status == 200) {
+            this.$store.commit("SET_AUTH_TOKEN", response.data.token);
+            this.$store.commit("SET_USER", response.data.user);
+            this.$router.push("/");
+          }
+        })
+        .catch((error) => {
+          const response = error.response;
+
+          if (response.status === 401) {
+            this.invalidCredentials = true;
+          }
+        });
+      UserService
+      .updateProfile(this.user)
+      .then((response) => {
+          if (response.status == 200) {
+            this.$router.push("/profile");
+          }
+        })
+        .catch((error) => {
+          const response = error.response;
+
+          if (response.status === 401) {
+            this.invalidCredentials = true;
+          }
+        });
     },
     resetForm(){
       this.edit = false;
--- vue/src/services/UserService.js
+++ vue/src/services/UserService.js
@@ -4,6 +4,10 @@ export default {
 
   profile(userId) {
     return axios.get(`/user/${userId}/profile`)
+  },
+
+  updateProfile(user){
+    return axios.put(`/user/${user.id}/profile`, user)
   }
 
 }
