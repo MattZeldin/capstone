bc9d70dca4ffa99d15a9cba5c86f6303a8a1fbe1
Aleah Duncan <alduncan501@gmail.com>
2023-08-11 13:46:33 -0400

    working on displaying workouts

--- vue/src/services/WorkoutService.js
+++ vue/src/services/WorkoutService.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 export default {
 
   getWorkouts(workout) {
-    return axios.get(`${workout.username}/workouts`)
+    return axios.get(`${workout.username}/workouts`)  
   },
   
   addWorkout(workout) {
