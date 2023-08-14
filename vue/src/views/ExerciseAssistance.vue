1b70fa2fc44a8d7f78795c058ee6978c016ea05d
Hannah Davis <131181656+davishl219@users.noreply.github.com>
2023-08-14 11:37:16 -0400

    Functioning Exercise Assistance view.

---
+++ vue/src/views/ExerciseAssistance.vue
@@ -0,0 +1,36 @@
+<template>
+  <div>
+      <exercise-card/>
+    <!-- <exercise-card
+        v-for="card in cards"
+        v-on:click="toggleCard(card)"
+        v-bind:key="card.id"
+        v-bind:card="card"
+    /> -->
+  </div>
+</template>
+
+<script>
+import ExerciseCard from "../components/ExerciseCard.vue";
+export default {
+  name: "exercise-assistance",
+//   data() {
+//     return {
+//       cards: [
+//         {
+//           id: 1,
+//           exercise: "",
+//           info: "",
+//           image: "",
+//         },
+//       ],
+//     };
+//   },
+  components: {
+    ExerciseCard,
+  },
+};
+</script>
+
+<style>
+</style>
