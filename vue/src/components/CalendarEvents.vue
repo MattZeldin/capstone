81bbc5bdca78d2aff51780b771185c348171c785
Hannah Davis <131181656+davishl219@users.noreply.github.com>
2023-08-15 10:14:25 -0400

    Calendar displays events from database.

--- vue/src/components/CalendarEvents.vue
+++ vue/src/components/CalendarEvents.vue
@@ -13,10 +13,10 @@ export default {
 components: {VueCal},
 data() {
   return {
-    events: this.$store.state.events 
   //   events: [
   //     {
   //       start: '2023-08-14 10:30',
+  //       // start: this.startFromStore,
   //       end: '2023-08-14 11:30',
   //       // You can also define event dates with Javascript Date objects:
   //       // start: new Date(2018, 11 - 1, 16, 10, 30),
@@ -24,10 +24,26 @@ data() {
   //       title: 'Doctor appointment',
   //       content: '<i class="icon material-icons">local_hospital</i>',
   //       class: 'health'
+  //     },
+  //     {
+  //       start: '2023-08-14 14:30',
+  //       // start: this.startFromStore,
+  //       end: '2023-08-14 15:30',
+  //       // You can also define event dates with Javascript Date objects:
+  //       // start: new Date(2018, 11 - 1, 16, 10, 30),
+  //       // end: new Date(2018, 11 - 1, 16, 11, 30),
+  //       title: 'Doctor appointment',
+  //       content: '<i class="icon material-icons">local_hospital</i>',
+  //       class: 'health'
   //     }
-  // ]
+  //  ]
   }
-}
+},
+computed: {
+    events() {
+      return this.$store.state.events
+    }
+  }
 }
 </script>
 
