ece1704bfd2c4a9aee1dbc06c8360f915be26283
Matt Zeldin <matthewzeldin@gmail.com>
2023-08-17 10:09:43 -0400

    idk what this was but I did it yesterday

--- vue/src/components/CalendarEvents.vue
+++ vue/src/components/CalendarEvents.vue
@@ -1,77 +1,79 @@
 <template>
-<div>
+  <div>
+    <vue-cal
+      :disable-views="['years', 'year']"
+      :events="events"
+      :on-event-click="onEventClick"
+      :twelveHour="true"
+    >
+    </vue-cal>
 
-<vue-cal
-  :disable-views="['years', 'year']"
-  :events="events"
-  :on-event-click="onEventClick"
-  :twelveHour="true">
-</vue-cal>
-
-<v-dialog v-model="showDialog">
-  <v-card>
-    <v-card-title>
-      <span>{{ selectedEvent.title }}</span>
-      <v-spacer/>
-      <strong>{{ selectedEvent.start && selectedEvent.start.formatTime()}}</strong>
-    </v-card-title>
-    <v-card-text>
-      <p v-html="selectedEvent.contentFull"/>
-      <strong>Event details:</strong>
-      <ul>
-        <li>Event starts at: {{ selectedEvent.start && selectedEvent.start.formatTime() }}</li>
-        <li>Event ends at: {{ selectedEvent.end && selectedEvent.end.formatTime() }}</li>
-        <!-- <li>{{selectedEvent.details}}</li> -->
-      </ul>
-    </v-card-text>
-  </v-card>
-</v-dialog>
-
-</div>
-
+    <v-dialog v-model="showDialog">
+      <v-card>
+        <v-card-title>
+          <span>{{ selectedEvent.title }}</span>
+          <v-spacer />
+          <strong>{{
+            selectedEvent.start && selectedEvent.start.formatTime()
+          }}</strong>
+        </v-card-title>
+        <v-card-text>
+          <p v-html="selectedEvent.contentFull" />
+          <strong>Event details:</strong>
+          <ul>
+            <li>
+              Event starts at:
+              {{ selectedEvent.start && selectedEvent.start.formatTime() }}
+            </li>
+            <li>
+              Event ends at:
+              {{ selectedEvent.end && selectedEvent.end.formatTime() }}
+            </li>
+            <!-- <li>{{selectedEvent.details}}</li> -->
+          </ul>
+        </v-card-text>
+      </v-card>
+    </v-dialog>
+  </div>
 </template>
 
 <script>
-import VueCal from 'vue-cal';
-import 'vue-cal/dist/vuecal.css';
-
+import VueCal from "vue-cal";
+import "vue-cal/dist/vuecal.css";
 
 export default {
-components: {VueCal},
-data() {
-  return {
+  components: { VueCal },
+  data() {
+    return {
       selectedEvent: {},
-      showDialog: false
-
-  }
-},
-computed: {
+      showDialog: false,
+    };
+  },
+  computed: {
     events() {
-      return this.$store.state.events
-    }
+      return this.$store.state.events;
+    },
   },
 
-  methods:  {  
-    onEventClick (event, e) {
-      this.selectedEvent = event
-      this.showDialog = !this.showDialog
+  methods: {
+    onEventClick(event, e) {
+      this.selectedEvent = event;
+      this.showDialog = !this.showDialog;
 
-      e.stopPropagation()
+      e.stopPropagation();
+    },
+
+    clickOutside() {
+      this.showDialog = false;
+    },
   },
-
-    clickOutside(){
-      this.showDialog = false
-    }
-
-
-   }
-}
+};
 </script>
 
 <style scoped src="../../node_modules/vuetify/dist/vuetify.min.css" >
-
-
-.vuecal__event {cursor: pointer;}
+.vuecal__event {
+  cursor: pointer;
+}
 
 .vuecal__event-title {
   font-size: 1.2em;
